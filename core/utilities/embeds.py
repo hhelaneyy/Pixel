60b83f905073f68bb4fc5aeec6959e534b6df260
Helaney1 <helaneyofficial@gmail.com>
2024-04-21 12:32:18 +0300

    Добавление NSFW картинок в embeds.py

--- core/utilities/embeds.py
+++ core/utilities/embeds.py
@@ -11,11 +11,26 @@ footer = [
     "Попробуй меня на вкус.",
     "Как зовут твоё имя?",
     "Какой пароль? Тайну раскрой.",
-    "Кто мы? Мы команда точек на экране!",
     "Газонокосилка.",
     "Пьеса без героя ничего не стоит.",
     "Моя любовь - не для этих кукол.",
     "Выпускай свой банкай...",
     "Я всегда в гармонии.",
     "...я уничтожу его одной левой!"
+]
+
+NSFW = [
+    'https://media.discordapp.net/attachments/1046501823898460230/1095756963289235547/15019452.gif?width=557&height=323',
+    'https://cdn.discordapp.com/attachments/1046501823898460230/1095756911942574090/hentay-na-telefon.gif',
+    'https://media.discordapp.net/attachments/1046501823898460230/1096040700455424060/7b2e59cebb2f-min.gif?width=539&height=304',
+    '"https://media.discordapp.net/attachments/1046501823898460230/1096040662253699183/Hjb-min.gif?width=719&height=516',
+    'https://media.discordapp.net/attachments/1046501823898460230/1096040699562033153/5e5a6a7ceaff-min.gif?width=539&height=330',
+    'https://media.discordapp.net/attachments/1046501823898460230/1095756910629769307/detail_2.gif?width=633&height=356',
+    'https://media.discordapp.net/attachments/1046501823898460230/1095756910629769307/detail_2.gif?width=633&height=356',
+    'https://media.discordapp.net/attachments/1046501823898460230/1096040701046816870/111-min.gif?width=647&height=431',
+    'https://images-ext-1.discordapp.net/external/G9WzYxYfB9WX0C-XL24rVvkPalzQfU3-vtJMLy-X3Fs/https/psv4.vkuseraudio.net/s/v1/d/MRyQgWm_dcJQDcJyrG4ZDyHv12fzm3PhOi0JXv03oD5orlmqRiVJUvv3LTjemocRgRWryGMSNuNpwt6w9pm2Sadgjsh0F4hV7jVmSz9RamgplF7Nq7Wqpg/UjZbxHn.gif?width=933&height=700',
+    'https://media.discordapp.net/attachments/1046501823898460230/1095756908931063818/1.gif?width=700&height=700',
+    'https://media.discordapp.net/attachments/1046501823898460230/1095756963641577612/76124401.gif?width=575&height=359',
+    'https://images-ext-1.discordapp.net/external/yMhk31G9VrKfBSF4fdmoXOnH1PS7Bqwm3pZuvGsQLyM/https/i.redd.it/6iq1odjvx2c71.gif?width=816&height=700',
+    'https://images-ext-1.discordapp.net/external/e8Zo8y3l58tg0ZJttQXnqfaRN89YRcjHbaI3xZ8tf8Q/https/i.redd.it/vhms1i0jh2541.gif?width=719&height=405v'
 ]
