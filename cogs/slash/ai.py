924565e55ee6ac9c84f1f8de3c566e5fcebd5313
Helaney <156517107+hhelaneyy@users.noreply.github.com>
2025-02-27 10:25:30 +0300

    secrets

--- cogs/slash/ai.py
+++ cogs/slash/ai.py
@@ -7,35 +7,6 @@ class OpenaiCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    '''@commands.slash_command(name="write", description="Задайте вопрос нейросети при помощи «GPT-3.5-Turbo».")
-    async def write(self, inter: disnake.ApplicationCommandInteraction, *, model: str = commands.Param(choices=['Kandinsky', 'ChatGPT 3.5 Turbo']), prompt):
-        author = inter.author
-
-        await inter.response.defer()
-        if model == 'ChatGPT 3.5 Turbo':
-            try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo-0613",
-                    messages=[
-                        {"role": "system", "content": prompt},
-                        {"role": "user", "content": prompt}
-                    ],
-                )
-                reply = response.choices[0].message.content
-                embed = disnake.Embed(title=f"⟩ Ответ нейросети:", description=reply, color=disnake.Color.blurple())
-                embed.set_footer(text=f"Поддерживается благодаря {self.bot.owner.name}", icon_url=self.bot.owner.avatar.url)
-                await inter.followup.send(embed=embed)
-
-            except Exception as e:
-                raise commands.CommandError(message='Невозможно сгенерировать ответ. Обратитесь к главному разработчику для выяснения проблемы.')
-
-        elif model == 'Kandinsky':
-            api = Text2ImageAPI('https://api-key.fusionbrain.ai/', '5963231F10FA2F5C48AFBD67FEE5440F', '2C2A0E9EB941E04B0DA386978042DB94')
-            model_id = api.get_model()
-            uuid = api.generate("Sun in sky", model_id)
-            images = api.check_generation(uuid)
-            print(images)'''
-
 class Text2ImageAPI:
     def __init__(self, url, api_key, secret_key):
         self.URL = url
